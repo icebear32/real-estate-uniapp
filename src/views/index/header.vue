e51b140aafcbc422cf7c586ae8fec2cc08997cbc
icebear32 <1727693137@qq.com>
2023-10-10 16:25:05 +0800

    搜索栏文字间歇滚动

--- src/views/index/header.vue
+++ src/views/index/header.vue
@@ -8,6 +8,12 @@
             <div class="search-box">
                 <i class="iconfont icon-sousuo"></i>
                 <input type="text">
+                <ul>
+                    <li>名企招聘</li>
+                    <li>买仓库</li>
+                    <li>商业房产</li>
+                    <li>附近超值保洁</li>
+                </ul>
                 <button>搜索</button>
             </div>
             <div class="search-sign">
