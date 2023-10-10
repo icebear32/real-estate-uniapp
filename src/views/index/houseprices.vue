2607384a4da8210cd8b54538bbae194eda7d9eed
icebear32 <1727693137@qq.com>
2023-10-11 00:15:24 +0800

    房价行情模块设计

---
+++ src/views/index/houseprices.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="houseprices">
+        <ul>
+            <li class="houseprices-state">
+                <img src="@/assets/images/houseprices.jpg" alt="">
+                <p>房价行情</p>
+            </li>
+            <li class="houseprices-price">
+                <div>
+                    <b>8888</b>元/m
+                    <sup>2</sup>
+                </div>
+                <p>最新均价</p>
+            </li>
+            <li class="houseprices-increase">
+                <img src="@/assets/images/up.png" alt="">
+                <span>0.12%</span>
+                <p>比上月</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped></style>
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -2,12 +2,14 @@
 import Header from './header.vue'
 import HeaderImg from './header-img.vue'
 import Navmenu from './navmenu.vue'
+import Houseprices from './houseprices.vue'
 </script>
 
 <template>
   <Header />
   <HeaderImg />
   <Navmenu />
+  <Houseprices />
 </template>
 
 <style scoped></style>
