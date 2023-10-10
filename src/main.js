64d2018e35ca6b275340688ae27e418f346c546a
icebear32 <1727693137@qq.com>
2023-10-10 21:27:51 +0800

    多行导航栏引入彩色图标和横向滚动

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './assets/css/index.css'
-import './assets/css/common.css'
-import './assets/font/iconfont.css'
+import '@/assets/css/index.css'
+import '@/assets/css/common.css'
+import '@/assets/font/iconfont.css'
+import '@/assets/font/iconfont'
 
 createApp(App).use(router).mount('#app')
--- src/views/index/navmenu.vue
+++ src/views/index/navmenu.vue
@@ -1,104 +1,40 @@
 <script setup>
+import { ref } from 'vue'
 
+let navmenu1 = ref([
+    { id: '1', title: '新房', icon: '#icon-zhuzhaixiaoqu' },
+    { id: '2', title: '租房', icon: '#icon-zufang' },
+    { id: '3', title: '二手房', icon: '#icon-ershoufangjiaoyi' },
+    { id: '4', title: '商铺写字楼', icon: '#icon-xiezilouSVG' },
+    { id: '5', title: '新车', icon: '#icon-icon270c_taxi' },
+    { id: '6', title: '新房', icon: '#icon-zhuzhaixiaoqu' },
+    { id: '7', title: '租房', icon: '#icon-zufang' },
+    { id: '8', title: '二手房', icon: '#icon-ershoufangjiaoyi' },
+    { id: '9', title: '商铺写字楼', icon: '#icon-xiezilouSVG' },
+    { id: '10', title: '新车', icon: '#icon-icon270c_taxi' },
+])
 </script>
 
 <template>
     <!-- 多行导航栏 -->
     <div class="navmenu">
         <div class="nav-1">
-            <div class="navmenu-left">
-                <i class="iconfont icon-maifang"></i>
-                <span>新房</span>
-            </div>
-            <div class="navmenu-left">
-                <i class="iconfont icon-maifang"></i>
-                <span>新房</span>
-            </div>
-            <div class="navmenu-left">
-                <i class="iconfont icon-maifang"></i>
-                <span>新房</span>
-            </div>
-            <div class="navmenu-left">
-                <i class="iconfont icon-maifang"></i>
-                <span>新房</span>
-            </div>
-
-            <div class="navmenu-left">
-                <i class="iconfont icon-maifang"></i>
-                <span>新房</span>
-            </div>
-            <div class="navmenu-left">
-                <i class="iconfont icon-maifang"></i>
-                <span>新房</span>
-            </div>
-            <div class="navmenu-left">
-                <i class="iconfont icon-maifang"></i>
-                <span>新房</span>
-            </div>
-            <div class="navmenu-left">
-                <i class="iconfont icon-maifang"></i>
-                <span>新房</span>
+            <div class="navmenu-left" v-for="v in navmenu1" :key="v.id">
+                <svg class="iconf-font">
+                    <use :xlink:href="v.icon"></use>
+                </svg>
+                <span>{{ v.title }}</span>
             </div>
         </div>
         <div class="nav-2">
             <div class="navmenu-left">
-                <i class="iconfont icon-xuexiao-"></i>
-                <span>查房价</span>
-            </div>
-            <div class="navmenu-left">
-                <i class="iconfont icon-xuexiao-"></i>
-                <span>查房价</span>
-            </div>
-            <div class="navmenu-left">
-                <i class="iconfont icon-xuexiao-"></i>
-                <span>查房价</span>
-            </div>
-            <div class="navmenu-left">
-                <i class="iconfont icon-xuexiao-"></i>
+                <svg class="iconf-font">
+                    <use xlink:href="#icon-xuexiao-"></use>
+                </svg>
                 <span>查房价</span>
             </div>
         </div>
     </div>
 </template>
 
-<style scoped>
-.navmenu {
-    /* background-color: #999; */
-    width: 100%;
-    overflow-x: auto;
-    white-space: nowrap;
-    min-height: 15rem;
-    position: relative;
-}
-
-.navmenu::-webkit-scrollbar {
-    display: none;
-}
-
-.nav-1,
-.nav-2 {
-    position: absolute;
-    top: 0;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-wrap: wrap;
-}
-
-.nav-1 {
-    left: 0%;
-}
-
-.nav-2 {
-    left: 100%;
-}
-
-.navmenu-left {
-    width: 25%;
-    text-align: center;
-}
-
-.navmenu-left span {
-    display: block;
-}
-</style>
+<style scoped></style>
