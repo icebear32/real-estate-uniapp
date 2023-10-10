2d8f3d7bcbf60f8564b75bef901710e15c6afcf1
icebear32 <1727693137@qq.com>
2023-10-10 20:54:19 +0800

    设计多行导航栏结构

--- src/views/index/navmenu.vue
+++ src/views/index/navmenu.vue
@@ -5,31 +5,100 @@
 <template>
     <!-- 多行导航栏 -->
     <div class="navmenu">
-        <div class="item">1111</div>
-        <div class="item">2222</div>
-        <div class="item">3333</div>
-        <div class="item">4444</div>
-        <div class="item">5555</div>
+        <div class="nav-1">
+            <div class="navmenu-left">
+                <i class="iconfont icon-maifang"></i>
+                <span>新房</span>
+            </div>
+            <div class="navmenu-left">
+                <i class="iconfont icon-maifang"></i>
+                <span>新房</span>
+            </div>
+            <div class="navmenu-left">
+                <i class="iconfont icon-maifang"></i>
+                <span>新房</span>
+            </div>
+            <div class="navmenu-left">
+                <i class="iconfont icon-maifang"></i>
+                <span>新房</span>
+            </div>
+
+            <div class="navmenu-left">
+                <i class="iconfont icon-maifang"></i>
+                <span>新房</span>
+            </div>
+            <div class="navmenu-left">
+                <i class="iconfont icon-maifang"></i>
+                <span>新房</span>
+            </div>
+            <div class="navmenu-left">
+                <i class="iconfont icon-maifang"></i>
+                <span>新房</span>
+            </div>
+            <div class="navmenu-left">
+                <i class="iconfont icon-maifang"></i>
+                <span>新房</span>
+            </div>
+        </div>
+        <div class="nav-2">
+            <div class="navmenu-left">
+                <i class="iconfont icon-xuexiao-"></i>
+                <span>查房价</span>
+            </div>
+            <div class="navmenu-left">
+                <i class="iconfont icon-xuexiao-"></i>
+                <span>查房价</span>
+            </div>
+            <div class="navmenu-left">
+                <i class="iconfont icon-xuexiao-"></i>
+                <span>查房价</span>
+            </div>
+            <div class="navmenu-left">
+                <i class="iconfont icon-xuexiao-"></i>
+                <span>查房价</span>
+            </div>
+        </div>
     </div>
 </template>
 
 <style scoped>
 .navmenu {
-    background-color: #999;
+    /* background-color: #999; */
     width: 100%;
     overflow-x: auto;
     white-space: nowrap;
-
-}
-
-.item {
-    display: inline-block;
-    width: 100px;
-    height: 100px;
-    margin: 10px;
+    min-height: 15rem;
+    position: relative;
 }
 
 .navmenu::-webkit-scrollbar {
     display: none;
+}
+
+.nav-1,
+.nav-2 {
+    position: absolute;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-wrap: wrap;
+}
+
+.nav-1 {
+    left: 0%;
+}
+
+.nav-2 {
+    left: 100%;
+}
+
+.navmenu-left {
+    width: 25%;
+    text-align: center;
+}
+
+.navmenu-left span {
+    display: block;
 }
 </style>
